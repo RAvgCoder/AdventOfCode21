da0429bf3886136950c1b538a248a6de7e03760f
Osebhulimen Egbor <os614875@dal.ca>
2024-09-08 14:57:55 -0300

    Day11 done

--- src/day11.rs
+++ src/day11.rs
@@ -1,6 +1,7 @@
-use crate::utils::coordinate::direction::FullDirection;
-use crate::utils::coordinate::Position;
+use crate::utils::coordinate_system::direction::FullDirection;
+use crate::utils::coordinate_system::Coordinate;
 use crate::utils::day_setup;
+use crate::utils::grid::sized_grid::SizedGrid;
 use day_setup::Utils;
 use std::fmt::Debug;
 
@@ -13,99 +14,115 @@ use std::fmt::Debug;
 ///   If the result of any part does not match the expected value.
 pub fn run() {
     // run_part(day_func_part_to_run, part_num, day_num)
-    Utils::run_part_single(part1, 1, 0, 0);
-    Utils::run_part(part2, 2, 0, 0);
+    Utils::run_part_single(part1, 1, 11, 1729);
+    Utils::run_part_single(part2, 2, 11, 237);
 }
-
+const GRID_SIZE: usize = 10;
 fn part1(mut octopus_grid: OctopusGrid) -> u64 {
-    println!("Part 1: {:#?}", octopus_grid);
-    let mut falshes = 0;
     for _ in 0..100 {
         octopus_grid.raise_energy_levels();
-        if octopus_grid.has_flashes() {
-            // flashses += octopus_grid.handle_flashes();
+        octopus_grid.process_flashes();
+    }
+
+    octopus_grid.num_flashes
+}
+
+fn part2(mut octopus_grid: OctopusGrid) -> u64 {
+    for i in 0.. {
+        octopus_grid.raise_energy_levels();
+        let all_flashing = octopus_grid.process_flashes();
+        if all_flashing {
+            return i + 1;
         }
     }
-    0
+    unreachable!("The octopus grid should have all grids flashing by now.")
 }
 
-fn part2(input: Vec<String>) -> u64 {
-    println!("Part 2 {:#?}", input);
-    0
-}
-
+#[derive(Debug)]
 struct OctopusGrid {
-    grid: [[EnergyLevel; 10]; 10],
-    curr_flashes: Vec<Position>,
+    grid: SizedGrid<EnergyLevel, GRID_SIZE, GRID_SIZE>,
+    curr_flashes: Vec<Coordinate>,
+    num_flashes: u64,
 }
 
 impl OctopusGrid {
-    pub(crate) fn handle_flashes(&mut self) -> u64 {
+    /// Handles the flashes of the octopuses in the grid.
+    ///
+    /// This function processes the current flashes in the grid by iterating through
+    /// the positions in `curr_flashes`. For each position, it checks all adjacent
+    /// positions in all directions. If an adjacent position contains an octopus that
+    /// is not already flashing and its energy level is raised to the flash level,
+    /// it is added to the `curr_flashes` queue. The total number of flashes is updated
+    /// accordingly.
+    ///
+    /// # Returns
+    /// `true` if all octopuses are flashing, otherwise `false`.
+    pub(crate) fn process_flashes(&mut self) -> bool {
+        let mut num_flashes = self.curr_flashes.len();
         while let Some(curr_position) = self.curr_flashes.pop() {
             for dir in FullDirection::full_direction_list() {
-                // let new_position = curr_position + dir;
-                // if let Some(e) = self.grid[new_position.i as usize][new_position.j as usize] {
-                //     e.raise_energy();
-                // }
-            }
-        }
-        0
-    }
-
-    fn raise_energy_levels(&mut self) {
-        self.curr_flashes.clear();
-        for (i, row) in self.grid.iter_mut().enumerate() {
-            for (j, energy) in row.iter_mut().enumerate() {
-                if energy.raise_energy() {
-                    self.curr_flashes.push(Position::new(i as i32, j as i32));
+                let new_position = curr_position + dir;
+                if let Some(e) = self.grid.get_mut(new_position) {
+                    if *e != EnergyLevel::Flash && e.raise_energy() {
+                        self.curr_flashes.push(new_position);
+                        self.num_flashes += 1;
+                        num_flashes += 1;
+                    }
                 }
             }
         }
+        num_flashes == GRID_SIZE * GRID_SIZE
     }
 
-    fn has_flashes(&self) -> bool {
-        !self.curr_flashes.is_empty()
+    /// Raises the energy levels of all octopuses in the grid.
+    ///
+    /// This function iterates through each octopus in the grid and raises its energy level.
+    /// If an octopus's energy level reaches the flash level, its position is added to the
+    /// `curr_flashes` queue, and the total number of flashes is incremented.
+    pub(crate) fn raise_energy_levels(&mut self) {
+        self.curr_flashes.clear();
+        for row in self.grid.iter_mut() {
+            for (position, energy) in row {
+                if energy.raise_energy() {
+                    self.curr_flashes.push(position);
+                    self.num_flashes += 1;
+                }
+            }
+        }
     }
 }
 
 impl From<Vec<String>> for OctopusGrid {
     fn from(value: Vec<String>) -> Self {
-        assert_eq!(value.len(), 10, "Invalid number of rows: {}", value.len());
+        assert_eq!(
+            value.len(),
+            GRID_SIZE,
+            "Invalid number of rows: {}",
+            value.len()
+        );
         assert!(
-            value.iter().all(|row| row.len() == 10),
+            value.iter().all(|row| row.len() == GRID_SIZE),
             "Invalid number of columns"
         );
 
-        let mut grid = [[EnergyLevel::ZERO; 10]; 10];
+        let mut grid = [[EnergyLevel::Flash; GRID_SIZE]; GRID_SIZE];
         for (i, row) in value.iter().enumerate() {
             for (j, e) in row.chars().enumerate() {
                 grid[i][j] = EnergyLevel::from(e);
             }
         }
         Self {
-            grid,
+            grid: SizedGrid::new(grid),
+            num_flashes: 0,
             curr_flashes: Vec::with_capacity(100),
         }
     }
 }
 
-impl Debug for OctopusGrid {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f)?;
-        for row in self.grid.iter() {
-            for e in row.iter() {
-                write!(f, "{:?} ", *e as u8)?;
-            }
-            writeln!(f)?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum EnergyLevel {
     /// FLASH MODE
-    ZERO = 0,
+    Flash = 0,
     One = 1,
     Two = 2,
     Three = 3,
@@ -129,7 +146,7 @@ impl EnergyLevel {
     pub(crate) fn raise_energy(&mut self) -> bool {
         let new_level = EnergyLevel::from(*self as u8 + 1);
         *self = new_level;
-        new_level == EnergyLevel::ZERO
+        new_level == EnergyLevel::Flash
     }
 }
 
@@ -162,8 +179,14 @@ impl From<u8> for EnergyLevel {
             7 => Self::Seven,
             8 => Self::Eight,
             9 => Self::Nine,
-            10 => Self::ZERO,
+            10 => Self::Flash,
             _ => panic!("Invalid energy level: {}", value),
         }
+    }
+}
+
+impl Debug for EnergyLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", *self as u8)
     }
 }
