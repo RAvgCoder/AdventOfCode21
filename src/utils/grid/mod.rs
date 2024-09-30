3ba5095fa9ef97ffaffc0109ac59c260e017e36c
Osebhulimen Egbor <os614875@dal.ca>
2024-09-30 15:00:21 -0300

    Added the iter and foreach function to the grid trait.

--- src/utils/grid/mod.rs
+++ src/utils/grid/mod.rs
@@ -1,4 +1,5 @@
 use crate::utils::coordinate_system::Coordinate;
+use crate::utils::grid::iterators::GridIter;
 
 mod grid_slice;
 pub mod sized_grid;
@@ -23,6 +24,47 @@ pub trait Grid<T> {
 
     /// Checks if the specified coordinate is valid within the grid.
     fn is_valid_coordinate(&self, coordinate: &Coordinate) -> bool;
+
+    /// Returns an iterator over the elements of the grid.
+    ///
+    /// # Type Parameters
+    /// * `'a` - The lifetime of the references to the grid and its elements.
+    ///
+    /// # Returns
+    /// A `GridIter` that iterates over the elements of the grid.
+    fn iter<'a>(&'a self) -> GridIter<'a, Self, T>
+    where
+        T: 'a,
+        Self: Sized;
+
+    /// Applies a function to each element in the grid.
+    ///
+    /// # Type Parameters
+    /// * `'a` - The lifetime of the references to the grid and its elements.
+    /// * `F` - The type of the function to apply to each element.
+    /// * `A` - The type of the result.
+    ///
+    /// # Arguments
+    /// * `func` - The function to apply to each element. It takes a `Coordinate`, a reference to the element, and a mutable reference to the result.
+    ///
+    /// # Returns
+    /// The result of applying the function to each element in the grid.
+    fn foreach<F, A>(&self, func: F) -> A
+    where
+        F: Fn(Coordinate, &T, &mut A),
+        A: Default,
+        Self: Sized,
+    {
+        let mut accumulator: A = Default::default();
+
+        for row in self.iter() {
+            for (pos, e) in row {
+                func(pos, e, &mut accumulator)
+            }
+        }
+
+        accumulator
+    }
 }
 
 #[allow(dead_code)]
@@ -74,11 +116,7 @@ pub mod iterators {
         /// Advances the iterator and returns the next row iterator.
         fn next(&mut self) -> Option<Self::Item> {
             if self.row < self.grid.num_rows() {
-                let row_iter = RowIter {
-                    row_item: self.grid.get_row(self.row),
-                    row: self.row,
-                    col: 0,
-                };
+                let row_iter = RowIter::new(self.grid.get_row(self.row), self.row, 0);
                 self.row += 1;
                 Some(row_iter)
             } else {
@@ -88,15 +126,32 @@ pub mod iterators {
     }
 
     /// An iterator over the elements of a row in a grid.
-    pub struct RowIter<'a, T> {
+    ///
+    /// # Type Parameters
+    /// * `'a` - The lifetime of the references to the row elements.
+    /// * `T` - The type of the elements in the row.
+    pub struct RowIter<'a, T>
+    where
+        T: 'a,
+    {
+        /// A reference to the slice of row elements.
         row_item: &'a [T],
+        /// The index of the current row.
         row: usize,
+        /// The index of the current column.
         col: usize,
+        /// A marker to indicate the lifetime of the row elements.
+        _marker: PhantomData<&'a T>,
     }
 
     impl<'a, T> RowIter<'a, T> {
         pub fn new(row_item: &'a [T], row: usize, col: usize) -> Self {
-            Self { row_item, row, col }
+            Self {
+                row_item,
+                row,
+                col,
+                _marker: PhantomData,
+            }
         }
     }
 
