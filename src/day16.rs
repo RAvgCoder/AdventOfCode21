e2ca6f323d7a4b28e664c1893e6d7c7001e9cd1a
Osebhulimen Egbor <os614875@dal.ca>
2024-10-08 21:44:52 -0300

    Day16 part 2 refactored

--- src/day16.rs
+++ src/day16.rs
@@ -29,7 +29,7 @@ fn part1(input: Vec<String>) -> u64 {
     packet.decode_version_number().version_number
 }
 
-fn part2(input: Vec<String>) -> u128 {
+fn part2(input: Vec<String>) -> u64 {
     let binary_strings = hex_to_binary_strings(input.first().unwrap());
     let mut packet = Packet {
         bits: &binary_strings,
@@ -43,20 +43,27 @@ fn part2(input: Vec<String>) -> u128 {
 
 #[derive(Debug)]
 struct Packet<'a> {
+    /// The bits representing the packet.
     bits: &'a str,
-    evaluated_expression: Option<u128>,
+    /// The evaluated expression value of the packet, if any.
+    evaluated_expression: Option<u64>,
 }
 
-
 impl<'a> Packet<'a> {
+    /// The range of bits representing the version number.
     const VERSION_NUMBER: RangeInclusive<usize> = 0..=2;
+    /// The range of bits representing the type ID.
     const TYPE_ID_RANGE: RangeInclusive<usize> = 3..=5;
 
-    ///110100101111111000101000
-    /// VVVTTTAAAAABBBBBCCCCC
+    /// Decodes the version number from the packet's bits.
     ///
-    /// 001 110 0 000000000011011 11010001010 0101001000100100 0000000
-    /// VVV TTT I LLLLLLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBBBBBBB
+    /// The version number is located in the first three bits of the packet.
+    ///
+    /// # Returns
+    /// A `PacketResult` containing the version number and the remaining bits.
+    ///
+    /// # Panics
+    /// Panics if the bits length is less than 6.
     fn decode_version_number(&mut self) -> PacketResult<'a> {
         let bits = self.bits;
         assert!(bits.len() >= 6, "Bits too short: {}", bits);
@@ -67,20 +74,16 @@ impl<'a> Packet<'a> {
             // base case
             let (evaluated_expression, rest) = Self::decode_literal(&bits[6..]);
 
-            self.evaluated_expression = Some(evaluated_expression as u128);
+            self.evaluated_expression = Some(evaluated_expression);
 
             PacketResult {
                 version_number,
                 rest,
             }
         } else {
+            let mut compute_fn =
+                Self::compute_from_type_id(type_id as usize, &mut self.evaluated_expression);
 
-            assert!(self.evaluated_expression.is_none(), "Evaluated expression should be None");
-            
-            // Initialize the evaluated expression to act as the accumulator and return the compute function
-            let compute_fn = Self::compute_from_type_id(type_id as usize, &mut self.evaluated_expression);
-
-            let mut acc = self.evaluated_expression.unwrap();
 
             if bits.as_bytes()[6] as char == '0' {
                 let offset = 7 + 15;
@@ -91,22 +94,26 @@ impl<'a> Packet<'a> {
                 let mut new_bits = &bits[..sub_packet_length as usize];
 
                 while !new_bits.is_empty() {
-                    let mut new_packet = Packet { bits: new_bits, evaluated_expression: None };
-                    let PacketResult { version_number, rest } = new_packet.decode_version_number();
+                    let mut new_packet = Packet {
+                        bits: new_bits,
+                        evaluated_expression: None,
+                    };
 
-                    acc = compute_fn(acc, new_packet.evaluated_expression.unwrap());
+                    let PacketResult {
+                        version_number,
+                        rest,
+                    } = new_packet.decode_version_number();
+
+                    compute_fn(new_packet.evaluated_expression.unwrap());
 
                     new_bits = rest;
                     acc_version_number += version_number;
                 }
 
-                self.evaluated_expression = Some(acc);
 
                 // Return the other packets that were not consumed in the fixed range
-                // (acc_version_number, &bits[sub_packet_length as usize..])
                 PacketResult {
                     version_number: acc_version_number,
-                    // evaluated_expression: acc_version_number,
                     rest: &bits[sub_packet_length as usize..],
                 }
             } else {
@@ -117,59 +124,124 @@ impl<'a> Packet<'a> {
                 let mut acc_version_number = version_number;
 
                 for _ in 0..sub_packet_length {
-                    let mut new_packet = Packet { bits, evaluated_expression: None };
-                    let PacketResult { version_number, rest } = new_packet.decode_version_number();
+                    let mut new_packet = Packet {
+                        bits,
+                        evaluated_expression: None,
+                    };
 
-                    acc = compute_fn(acc, new_packet.evaluated_expression.unwrap());
+                    let PacketResult {
+                        version_number,
+                        rest,
+                    } = new_packet.decode_version_number();
+
+                    compute_fn(new_packet.evaluated_expression.unwrap());
 
                     acc_version_number += version_number;
                     bits = rest;
                 }
 
-                self.evaluated_expression = Some(acc);
 
-                // (acc_version_number, bits)
                 PacketResult {
                     version_number: acc_version_number,
-                    // evaluated_expression: acc_version_number,
                     rest: bits,
                 }
             }
         }
     }
 
-    fn compute_from_type_id(type_id: usize, acc: &mut Option<u128>) -> impl Fn(u128, u128) -> u128 {
-        type ComputeTool = (u128, fn(u128, u128) -> u128);
-        const FUNCTIONS: [ComputeTool; 7] = [
-            (0, |a: u128, b| a + b),          // type_id 0
-            (1, |a: u128, b| a * b),          // type_id 1
-            (u128::MAX, |a: u128, b| a.min(b)),       // type_id 2
-            (0, |a: u128, b| a.max(b)),       // type_id 3
-            (u128::MAX, |a, b| {
-                if a == u128::MAX { return b; }
-                if a > b { 1_u128 } else { 0 }
-            }),  // type_id 5
-            (u128::MAX, |a, b| {
-                if a == u128::MAX { return b; }
-                if a < b { 1_u128 } else { 0 }
-            }),  // type_id 6
-            (u128::MAX, |a, b| {
-                if a == u128::MAX { return b; }
-                if a == b { 1_u128 } else { 0 }
-            }), // type_id 7
-        ];
-
-        let result = match type_id {
-            0..=3 => FUNCTIONS[type_id],
-            5..=7 => FUNCTIONS[type_id - 1],
+    
+    /// Returns a closure that modifies the accumulator based on the `type_id`.
+    ///
+    /// # Arguments
+    /// * `type_id` - The type ID of the packet.
+    /// * `acc` - A mutable reference to an optional accumulator value.
+    ///
+    /// # Panics
+    /// Panics if the accumulator is not `None`.
+    ///
+    /// # Returns
+    /// A closure that takes a `u64` value and modifies the accumulator.
+    fn compute_from_type_id(type_id: usize, acc: &mut Option<u64>) -> Box<dyn FnMut(u64) + '_> {
+        assert!(acc.is_none(), "Accumulator should be None");
+        match type_id {
+            0 => {
+                *acc = Some(0);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    *acc_ref += b;
+                })
+            }
+            1 => {
+                *acc = Some(1);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    *acc_ref *= b;
+                })
+            }
+            2 => {
+                *acc = Some(u64::MAX);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    *acc_ref = (*acc_ref).min(b);
+                })
+            }
+            3 => {
+                *acc = Some(0);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    *acc_ref = (*acc_ref).max(b);
+                })
+            }
+            5 => {
+                *acc = Some(u64::MAX);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    if *acc_ref == u64::MAX {
+                        *acc_ref = b;
+                    } else if *acc_ref > b {
+                        *acc_ref = 1;
+                    } else {
+                        *acc_ref = 0;
+                    }
+                })
+            }
+            6 => {
+                *acc = Some(u64::MAX);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    if *acc_ref == u64::MAX {
+                        *acc_ref = b;
+                    } else if *acc_ref < b {
+                        *acc_ref = 1;
+                    } else {
+                        *acc_ref = 0;
+                    }
+                })
+            }
+            7 => {
+                *acc = Some(u64::MAX);
+                Box::new(|b: u64| {
+                    let acc_ref = acc.as_mut().unwrap();
+                    if *acc_ref == u64::MAX {
+                        *acc_ref = b;
+                    } else if *acc_ref == b {
+                        *acc_ref = 1;
+                    } else {
+                        *acc_ref = 0;
+                    }
+                })
+            }
             _ => unreachable!("Invalid type_id: {}", type_id),
-        };
-
-        *acc = Some(result.0);
-
-        result.1
+        }
     }
 
+    /// Decodes a literal value from the packet's bits.
+    ///
+    /// # Arguments
+    /// * `sub_bits` - A string slice representing the bits to decode.
+    ///
+    /// # Returns
+    /// A tuple containing the decoded literal value and the remaining bits.
     fn decode_literal(mut sub_bits: &str) -> (u64, &str) {
         let mut acc = String::with_capacity(sub_bits.len());
         loop {
@@ -183,6 +255,13 @@ impl<'a> Packet<'a> {
         }
     }
 
+    /// Converts a binary string to an integer.
+    ///
+    /// # Arguments
+    /// * `binary_string` - A string slice representing the binary string.
+    ///
+    /// # Returns
+    /// A `u64` value representing the integer.
     fn binary_str_to_int(binary_string: &str) -> u64 {
         u64::from_str_radix(binary_string, 2).unwrap()
     }
